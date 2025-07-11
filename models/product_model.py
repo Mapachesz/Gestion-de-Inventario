4fbb12692c84175f51b13d72683d1ced23ff7583
Mapachesz <Lucastbernards@gmail.com>
2025-07-11 17:13:32 -0400

    FEATURE: Agergar un producto

---
+++ models/product_model.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+from datetime import date
+
+class Producto(BaseModel):
+    codigo: str = Field(..., min_length=1)
+    nombre: str
+    descripcion: Optional[str] = None
+    stock: int = Field(..., ge=0)
+    precio_unitario: float = Field(..., gt=0)
+    fecha_ingreso: Optional[date] = None
